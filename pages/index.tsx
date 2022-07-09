32bb79abfe6a006df3a1c6f27f7d25238bf10b8e
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2022-07-09 22:39:26 +0900

    practice tailwindcss 2

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,7 +21,28 @@ const Home: NextPage = () => {
           Checkout
         </div>
       </div>
-      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
+      <div className="bg-white overflow-hidden rounded-2xl shadow-xl">
+        <div className="bg-blue-500 p-6 pb-14">
+          <span className="text-white text-2xl">Profile</span>
+        </div>
+        <div className="rounded-3xl p-6 bg-white relative -top-5">
+          <div className="flex justify-between items-end relative -top-16">
+            <div className="flex flex-col items-center">
+              <span className="text-sm text-gray-500">Orders</span>
+              <span className="font-medium">340</span>
+            </div>
+            <div className="h-24 w-24 bg-red-400 rounded-full" />
+            <div className="flex flex-col items-center">
+              <span className="text-sm text-gray-500">Spent</span>
+              <span className="font-medium">$340</span>
+            </div>
+          </div>
+          <div className="relative flex flex-col items-center -mt-10 -mb-5">
+            <span className="text-lg font-medium">Tony Molloy</span>
+            <span className="text-sm text-gray-500">한국</span>
+          </div>
+        </div>
+      </div>
       <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
       <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
     </div>

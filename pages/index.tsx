e6c337eb60cdf85e01154fd05d2fbb3d220fe979
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2022-07-11 18:17:53 +0900

    Add tailwindcss 4

--- pages/index.tsx
+++ pages/index.tsx
@@ -92,7 +92,36 @@ const Home: NextPage = () => {
         </div>
       </div>
 
-      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
+      <div className="bg-white p-10 rounded-2xl shadow-xl">
+        <form className="flex flex-col space-y-2 bg-blue-500 p-5 focus-within:bg-blue-100">
+          <input
+            type="text"
+            required
+            placeholder="Username"
+            className="required:bg-yellow-500 invalid:bg-red-500 valid:bg-teal-500"
+          />
+          <input type="password" required placeholder="Password" />
+          <input type="submit" value="Login" className="bg-white" />
+        </form>
+      </div>
+
+      <div className="bg-white p-10 rounded-2xl shadow-xl">
+        <form className="flex flex-col space-y-2 p-5">
+          <input
+            type="text"
+            required
+            placeholder="Username"
+            className="border p-1 peer border-gray-400 rounded-lg"
+          />
+          <span className="hidden peer-invalid:block peer-invalid:text-red-500">
+            This input is invalid
+          </span>
+          <span className="hidden peer-valid:block peer-valid:text-teal-500">
+            Awesome username
+          </span>
+          <input type="submit" value="Login" className="bg-white" />
+        </form>
+      </div>
     </div>
   );
 };

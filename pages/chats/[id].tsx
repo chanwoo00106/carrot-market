f3e752ea61c769362c3641f9b1b0cd2c8a7189a4
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2022-07-21 22:13:21 +0900

    Add chat detail page

---
+++ pages/chats/[id].tsx
@@ -0,0 +1,41 @@
+import type { NextPage } from "next";
+
+const ChatDetail: NextPage = () => {
+  return (
+    <div className="py-10 px-4 space-y-4">
+      <div className="flex items-center space-x-2">
+        <div className="w-8 h-8 rounded-full bg-slate-400" />
+        <div className="w-1/2 text-gray-700 p-2 border border-gray-400 rounded-md">
+          <p>Hi how much are you selling them for?</p>
+        </div>
+      </div>
+      <div className="flex flex-row-reverse items-center space-x-2 space-x-reverse">
+        <div className="w-8 h-8 rounded-full bg-slate-400" />
+        <div className="w-1/2 text-gray-700 p-2 border border-gray-400 rounded-md">
+          <p>I want ￦20,000</p>
+        </div>
+      </div>
+      <div className="flex items-center space-x-2">
+        <div className="w-8 h-8 rounded-full bg-slate-400" />
+        <div className="w-1/2 text-gray-700 p-2 border border-gray-400 rounded-md">
+          <p>미쳤어</p>
+        </div>
+      </div>
+      <div className="fixed w-full mx-auto max-w-md bottom-2 left-0 right-0">
+        <div className="flex items-center relative">
+          <input
+            type="text"
+            className="shadow-sm rounded-full w-full border-gray-300 focus:ring-orange-500 focus:outline-none pr-12 focus:border-orange-500"
+          />
+          <div className="absolute top-0 bottom-0 flex py-1.5 pr-1.5 right-0">
+            <button className="flex focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 items-center bg-orange-500 hover:bg-orange-600 rounded-full px-3 text-sm text-white">
+              &rarr;
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChatDetail;

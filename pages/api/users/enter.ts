ca32fc3c5fde040a5c319d451cd2ba75381d7e4f
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2022-08-03 21:55:50 +0900

    Send SMS

--- pages/api/users/enter.ts
+++ pages/api/users/enter.ts
@@ -1,14 +1,17 @@
+import twilio from "twilio";
 import type { NextApiRequest, NextApiResponse } from "next";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
 import client from "@libs/server/client";
+
+const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
 async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
-  const data = req.body;
+  const { email, phone } = req.body;
 
-  if (!data.email && !data.phone)
+  if (!email && !phone)
     return res.status(400).json({ ok: false, message: "BadRequest" });
 
   const payload = Math.floor(100000 + Math.random() * 900000) + "";
@@ -18,12 +21,22 @@ async function handler(
       payload,
       user: {
         connectOrCreate: {
-          where: { ...data },
-          create: { name: "Anonymous", ...data },
+          where: { ...(email ? { email } : { phone: +phone }) },
+          create: {
+            name: "Anonymous",
+            ...(email ? { email } : { phone: +phone }),
+          },
         },
       },
     },
   });
+
+  if (phone)
+    await twilioClient.messages.create({
+      messagingServiceSid: process.env.TWILIO_MSID,
+      to: process.env.MY_PHONE!,
+      body: `Your login token is ${payload}.`,
+    });
 
   return res.json({ ok: true });
 }

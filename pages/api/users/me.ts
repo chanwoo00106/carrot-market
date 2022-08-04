03ea527afc574ac8859e32cb9c077d89e5494136
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2022-08-04 20:53:21 +0900

    Add Profile handler

--- pages/api/users/me.ts
+++ pages/api/users/me.ts
@@ -1,5 +1,4 @@
 import { withIronSessionApiRoute } from "iron-session/next";
-import twilio from "twilio";
 import type { NextApiRequest, NextApiResponse } from "next";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
 import client from "@libs/server/client";
@@ -16,26 +15,13 @@ async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
-  const { token } = req.body;
-  const exists = await client.token.findUnique({
-    where: { payload: token },
-    include: { user: true },
+  const profile = await client.user.findFirst({
+    where: { id: req.session.user?.id },
   });
-
-  if (!exists) return res.status(401).json({ ok: false });
-
-  req.session.user = {
-    id: exists.userId,
-  };
-
-  await req.session.save();
-
-  console.log(req.session);
-
-  return res.status(200).json({ ok: true });
+  return res.status(200).json({ ok: true, profile });
 }
 
-export default withIronSessionApiRoute(withHandler("POST", handler), {
+export default withIronSessionApiRoute(withHandler("GET", handler), {
   cookieName: "carrotsession",
   password: process.env.SESSION_PASSWORD!,
 });

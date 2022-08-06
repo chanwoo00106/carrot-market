a74315567788776dea4be73293aae8d89efb565c
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2022-08-06 23:02:47 +0900

    Update times/upload page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Layout from "@components/layout";
 import useUser from "@libs/client/useUser";
+import Link from "next/link";
 
 const Home: NextPage = () => {
   const { user, isLoading } = useUser();
@@ -63,23 +64,27 @@ const Home: NextPage = () => {
           </div>
         ))}
       </div>
-      <button className="fixed hover:bg-orange-500 transition-colors bottom-24 right-5 shadow-xl bg-orange-400 rounded-full p-4 text-white">
-        <svg
-          className="h-6 w-6"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          aria-hidden="true"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-          />
-        </svg>
-      </button>
+      <Link href="/items/upload">
+        <a>
+          <button className="fixed hover:bg-orange-500 transition-colors bottom-24 right-5 shadow-xl bg-orange-400 rounded-full p-4 text-white">
+            <svg
+              className="h-6 w-6"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+              />
+            </svg>
+          </button>
+        </a>
+      </Link>
     </Layout>
   );
 };
--- pages/items/upload.tsx
+++ pages/items/upload.tsx
@@ -1,11 +1,27 @@
 import type { NextPage } from "next";
 import Button from "@components/Button";
 import Layout from "@components/layout";
+import { useForm } from "react-hook-form";
+import { useMutation } from "@libs/";
+
+interface UploadProductForm {
+  name: string;
+  price: string;
+  description: string;
+}
 
 const Upload: NextPage = () => {
+  const { register, handleSubmit } = useForm<UploadProductForm>();
+  const [uploadProduct, { loading, data }] = useMutation("/api/products");
+
+  const onValid = async (data: UploadProductForm) => {
+    if (loading) return;
+    console.log(data);
+  };
+
   return (
-    <Layout canGoBack>
-      <div className="space-y-5 px-4 py-16">
+    <Layout canGoBack title="Upload Product">
+      <form onSubmit={handleSubmit(onValid)} className="space-y-5 px-4 py-16">
         <div>
           <label className="w-full cursor-pointer text-gray-600 hover:text-orange-500 hover:border-orange-500 flex items-center justify-center border-2 border-dashed border-gray-300 py-6 h-48 rounded-md">
             <svg
@@ -35,9 +51,9 @@ const Upload: NextPage = () => {
           <div className="rounded-md relative flex  items-center shadow-sm">
             <input
               id="name"
-              type="email"
+              type="text"
               className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
-              required
+              {...register("name", { required: true })}
             />
           </div>
         </div>
@@ -57,6 +73,7 @@ const Upload: NextPage = () => {
               id="price"
               type="text"
               placeholder="0.00"
+              {...register("price", { required: true })}
             />
             <div className="absolute right-0 pointer-events-none pr-3 flex items-centerj">
               <span className="text-gray-500">USD</span>
@@ -70,10 +87,11 @@ const Upload: NextPage = () => {
           <textarea
             className="mt-1 shadow-sm w-full focus:ring-orange-500 rounded-md border-gray-300 focus:border-orange-500"
             rows={4}
+            {...register("description")}
           />
         </div>
-        <Button>Upload product</Button>
-      </div>
+        <Button>{loading ? "Loading..." : "Upload product"}</Button>
+      </form>
     </Layout>
   );
 };

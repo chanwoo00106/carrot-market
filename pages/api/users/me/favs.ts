458a348be191c544b73c36747e23638a824596d1
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2022-08-10 17:13:28 +0900

    Add handler

---
+++ pages/api/users/me/favs.ts
@@ -0,0 +1,29 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import { withApiSession } from "@libs/server/withSession";
+import client from "@libs/server/client";
+
+declare module "iron-session" {
+  interface IronSessionData {
+    user?: {
+      id: number;
+    };
+  }
+}
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const {
+    session: { user },
+  } = req;
+
+  const favs = await client.fav.findMany({
+    where: { id: user?.id },
+    include: { product: true },
+  });
+  return res.status(200).json({ ok: true, favs });
+}
+
+export default withApiSession(withHandler({ methods: ["GET"], handler }));

2b34963fc9ae133f92aa9a194cc82897b71052ac
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2022-08-13 21:41:49 +0900

    Add upload form

--- pages/stream/create.tsx
+++ pages/stream/create.tsx
@@ -1,11 +1,36 @@
 import { NextPage } from "next";
 import Button from "@components/Button";
 import Layout from "@components/layout";
+import { useForm } from "react-hook-form";
+import { useMutation } from "@libs/index";
+import { useRouter } from "next/router";
+import { Stream } from "@prisma/client";
+
+interface CreateForm {
+  name: string;
+  price: number;
+  description: string;
+}
+
+interface StreamResponse {
+  ok: boolean;
+  stream: Stream;
+}
 
 const Create: NextPage = () => {
+  const { register, handleSubmit } = useForm<CreateForm>();
+  const [create, { data, loading }] =
+    useMutation<StreamResponse>("/api/streams");
+  const router = useRouter();
+
+  const onValid = async (form: CreateForm) => {
+    await create(form);
+    router.replace(`/stream/${data?.stream.id}`);
+  };
+
   return (
     <Layout canGoBack>
-      <div className="space-y-4 py-10 px-4">
+      <form onSubmit={handleSubmit(onValid)} className="space-y-4 py-10 px-4">
         <div>
           <label
             className="mb-1 block text-sm font-medium text-gray-700"
@@ -15,8 +40,9 @@ const Create: NextPage = () => {
           </label>
           <div className="rounded-md relative flex  items-center shadow-sm">
             <input
+              {...register("name", { required: true })}
               id="name"
-              type="email"
+              type="text"
               className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
               required
             />
@@ -34,6 +60,7 @@ const Create: NextPage = () => {
               <span className="text-gray-500 text-sm">$</span>
             </div>
             <input
+              {...register("price", { required: true })}
               className="appearance-none pl-7 w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
               id="price"
               type="text"
@@ -49,12 +76,13 @@ const Create: NextPage = () => {
             Description
           </label>
           <textarea
+            {...register("description", { required: true })}
             className="mt-1 shadow-sm w-full focus:ring-orange-500 rounded-md border-gray-300 focus:border-orange-500"
             rows={4}
           />
         </div>
-        <Button>Go live</Button>
-      </div>
+        <Button>{loading ? "Loading..." : "Go Live"}</Button>
+      </form>
     </Layout>
   );
 };
--- pages/stream/index.tsx
+++ pages/stream/index.tsx
@@ -1,5 +1,6 @@
 import { NextPage } from "next";
 import Layout from "@components/layout";
+import Link from "next/link";
 
 const Stream: NextPage = () => {
   return (
@@ -13,22 +14,26 @@ const Stream: NextPage = () => {
             </h3>
           </div>
         ))}
-        <button className="fixed hover:bg-orange-500 transition-colors bottom-24 right-5 shadow-xl bg-orange-400 rounded-full p-4 text-white">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            strokeWidth="2"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z"
-            />
-          </svg>
-        </button>
+        <Link href={"/stream/create"}>
+          <a>
+            <button className="fixed hover:bg-orange-500 transition-colors bottom-24 right-5 shadow-xl bg-orange-400 rounded-full p-4 text-white">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth="2"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z"
+                />
+              </svg>
+            </button>
+          </a>
+        </Link>
       </div>
     </Layout>
   );

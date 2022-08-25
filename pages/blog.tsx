33d35da301577f0e7664cdd12d728ef728618d1f
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2022-08-25 18:23:03 +0900

    Add getStaticProps

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,12 +1,46 @@
 import Layout from "@components/layout";
-import { NextPage } from "next";
+import { readdirSync, readFileSync } from "fs";
+import { NextPage, GetStaticProps } from "next";
+import matter from "gray-matter";
 
-const Blog: NextPage = () => {
+interface Post {
+  title: string;
+  date: string;
+  category: string;
+}
+
+const Blog: NextPage<{ posts: Post[] }> = ({ posts }) => {
   return (
     <Layout title="Blog" seoTitle="Blog">
-      <h1 className="font-semibold text-lg">Latest Posts</h1>
+      <h1 className="font-semibold text-lg mb-10 text-center mt-5">
+        Latest Posts
+      </h1>
+      <ul>
+        {posts.map((post, i) => (
+          <div key={i} className="mb-5">
+            <span className="text-lg text-red-500">{post.title}</span>
+            <div>
+              <span>
+                {post.date} / {post.category}
+              </span>
+            </div>
+          </div>
+        ))}
+      </ul>
     </Layout>
   );
+};
+
+export const getStaticProps: GetStaticProps = () => {
+  const posts = readdirSync("./posts").forEach((file) => {
+    const content = readFileSync(`./posts/${file}`, "utf-8");
+    return matter(content).data;
+  });
+  return {
+    props: {
+      posts,
+    },
+  };
 };
 
 export default Blog;
--- posts/01-first-post.md
+++ posts/01-first-post.md
@@ -1,3 +1,9 @@
 # Welcome
 
+---
+title: Welcome
+date: 2022.05.25
+category: thougths
+---
+
 This is my first blog post!
